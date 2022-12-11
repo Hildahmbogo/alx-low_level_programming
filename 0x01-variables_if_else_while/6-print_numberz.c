e7d5f4351deadf31deafc25b44440b82ada0965d
Hildahmbogo <mbogohildah@gmail.com>
2022-12-11 09:36:24 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
- *
+ *  
  * Return: 0
  */
 int main(void)
 {
-	int n;
-	for (n = 48; n < 58; n++)
-		putchar(n);
-	putchar('\n');
-	return (0);
+		int i = 48;
+
+		while (i < 58)
+		{
+			putchar(i);
+			i++;
+		}
+			putchar('\n');
+			return (0);
 }
