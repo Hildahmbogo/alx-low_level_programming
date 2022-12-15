750afa30258a17351af04f6d9b7519b7fc70b1fb
Hildahmbogo <mbogohildah@gmail.com>
2022-12-15 11:58:06 -0800

    tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,29 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-void print_times_table(int n)
+int main(void)
 {
-	int num, mult, prod;
+	long int i, j, k, next;
 
-	if (n >= 0 && n <= 15)
+	j = 1;
+	k = 2;
+	for (i = 1; i <= 50; ++i)
 	{
-		for (num = 0; num <= n; num++)
+		if (j != 20365011074)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
 		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
+	return (0);
 }
 
